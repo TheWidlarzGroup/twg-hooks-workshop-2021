a4176e1bfdc29d80b76290a745a211d9c324804a
Piter Bedlechowicz <piter@thewidlarzgroup.com>
2021-08-16 13:54:47 +0200

    Add storybook structure for useMemo and useRef

---
+++ src/stories/Hooks/Memo.stories.tsx
@@ -0,0 +1,7 @@
+export default {
+  title: 'Hooks/useMemo',
+}
+
+export * from '../../hooks/memo/MemoizedHeavyCalculationWithCache'
+export * from '../../hooks/memo/MemoizedHeavyCalculation'
+export * from '../../hooks/memo/ReferenceChangeExamples'
---
+++ src/stories/Hooks/Ref.stories.tsx
@@ -0,0 +1,9 @@
+export default {
+  title: 'Hooks/useRef',
+}
+
+export * from '../../hooks/ref/CustomComponentWithRefButRenamed'
+export * from '../../hooks/ref/refWithHook'
+export * from '../../hooks/ref/ForwardedComponentWithImperativeHandle'
+export * from '../../hooks/ref/WithForwardedRef'
+export * from '../../hooks/ref/withoutForwardRef'
