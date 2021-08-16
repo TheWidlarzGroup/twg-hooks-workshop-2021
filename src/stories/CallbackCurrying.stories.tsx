741f6e5c66000dd2ad8b7b488d807625207b190e
KacperM <kacpermikolajczak10@gmail.com>
2021-08-16 13:51:52 +0200

    add currying callbacks forwardref

---
+++ src/stories/CallbackCurrying.stories.tsx
@@ -0,0 +1,5 @@
+export default {
+  title: 'Example/Curring'
+}
+
+export * from '../hooks/callback/limitations/curring'
---
+++ src/stories/ForwardRef.stories.tsx
@@ -0,0 +1,13 @@
+import { WithoutForwardRefButRenamed } from '../hooks/ref/CustomComponentWithRefButRenamed'
+import { WithForwardedRef } from '../hooks/ref/WithForwardedRef'
+import { WithoutForwardRef } from '../hooks/ref/withoutForwardRef'
+
+export default { 
+    title: "Example/ForwardRef"
+}
+
+export {
+    WithForwardedRef,
+    WithoutForwardRef,
+    WithoutForwardRefButRenamed
+}
